b6069db57e75e5dad1ceed93d3a8d7b783bd8e4b
Android <android@Androids-MacBook-Pro.local>
2021-03-03 11:00:36 -0800

    Add navigation graph

--- app/src/main/java/com/example/dawnlightclinicalstudy/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dawnlightclinicalstudy/presentation/MainActivity.kt
@@ -1,38 +1,12 @@
 package com.example.dawnlightclinicalstudy.presentation
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.dawnlightclinicalstudy.ui.theme.DawnlightClinicalStudyTheme
+import com.example.dawnlightclinicalstudy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DawnlightClinicalStudyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DawnlightClinicalStudyTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
